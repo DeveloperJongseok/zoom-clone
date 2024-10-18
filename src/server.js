77d9c8bf1930c5a9b8a0f0f46c5c0623cbc91fcf
DeveloperJongseok <developerjongseok2024@gmail.com>
2024-10-18 11:20:45 +0900

    Setting WebSocket

--- src/server.js
+++ src/server.js
@@ -1,13 +1,32 @@
 import express from 'express';
+import WebSocket, { WebSocketServer } from 'ws';
+import http from 'http';
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
-app.get('/', (req, res) => res.render('home'));
-app.get('/*', (req, res) => res.redirect('/'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
 
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (socket) => {
+  console.log('Connected to Browser');
+  socket.on('close', () => {
+    console.log('Disconnected from the Browser');
+  });
+  socket.on('message', (message) => {
+    console.log(message.toString('utf8'));
+  });
+  socket.send('hi~');
+});
+
+server.listen(3000, handleListen);
